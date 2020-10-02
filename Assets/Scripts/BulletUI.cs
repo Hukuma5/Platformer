4a73bf3cbd2aed8fa9f1de7a3640840cd9118b6e
avel0041 <56276244+avel0041@users.noreply.github.com>
2020-10-02 19:50:20 +0300

    Add some mechanics

---
+++ Assets/Scripts/BulletUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletUI : MonoBehaviour {
+    private Rigidbody2D rb;
+    public float maxSpeed = 5f;
+    public GameObject Respawn;
+    public GameObject Enemy;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = Respawn.transform.position;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (rb.position.x < 40)
+        {
+            rb.transform.position = Enemy.transform.position;
+        }
+        rb.velocity = new Vector2(-maxSpeed, 0);
+    }
+}
---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour {
+    public float maxSpeed = 5f;
+    private bool isFacingRight = true;
+    private Animator anim;
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+    public Transform groundCheck;
+    private float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", isGrounded);
+        anim.SetFloat("vSpeed", rb.velocity.y);
+        //if (!isGrounded)
+        //    return;
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));
+        rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
+        if (move > 0 && !isFacingRight)
+            Flip();
+        else if (move < 0 && isFacingRight)
+            Flip();
+    }
+
+    private void Update()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            rb.AddForce(new Vector2(0, 62));
+        }
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
